604fa45df16f6e85506268b4e97c6ec93ec0b15b
Diogo Marto <diogo.marto@ua.pt>
2023-06-06 18:53:44 +0100

    cenas jonh

--- THEBIGBOY.sql
+++ THEBIGBOY.sql
@@ -1581,6 +1581,21 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE addMissionToProgram
+	@Program INTEGER,
+	@MissionID INTEGER
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	INSERT INTO ProgramHasMission(Prog_ID,Mission_ID)
+		VALUES (@Program,@MissionID)
+END
+GO
+
+
 CREATE PROCEDURE addModule
     @Type VARCHAR(16),
     @Description VARCHAR(256) ,
